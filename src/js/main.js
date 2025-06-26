f591b48df82c8f62add0c46c409a3f3fe0e74998
KalyanovAnton <kalyanov.anton@gmail.com>
2025-06-26 14:30:49 +0300

    footer1.2

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 import '../partials/header-example/01-header-example.js';
 import '../partials/home-page/home-page.js';
 import '../partials/hero-page/hero-page.js';
+import '../partials/Footer/js/footer.js';
